9cd77af91fec5e2c308203026d12acda164d82fa
ZXY <1105237442@qq.com>
2026-05-21 01:16:45 +0800

    feat: add /chat routes to user portal

--- frontend/packages/user-portal/src/router.tsx
+++ frontend/packages/user-portal/src/router.tsx
@@ -4,6 +4,7 @@ import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
 import UploadPage from './pages/UploadPage';
 import ReportDetailPage from './pages/ReportDetailPage';
+import ChatPage from './pages/ChatPage';
 import ProfilePage from './pages/ProfilePage';
 
 function AuthGuard({ children }: { children: React.ReactNode }) {
@@ -18,6 +19,8 @@ export const AppRouter = () => (
     <Route path="/" element={<AuthGuard><HomePage /></AuthGuard>} />
     <Route path="/upload" element={<AuthGuard><UploadPage /></AuthGuard>} />
     <Route path="/report/:id" element={<AuthGuard><ReportDetailPage /></AuthGuard>} />
+    <Route path="/chat" element={<AuthGuard><ChatPage /></AuthGuard>} />
+    <Route path="/chat/:sessionId" element={<AuthGuard><ChatPage /></AuthGuard>} />
     <Route path="/profile" element={<AuthGuard><ProfilePage /></AuthGuard>} />
     <Route path="*" element={<Navigate to="/" replace />} />
   </Routes>
